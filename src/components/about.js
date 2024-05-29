5babac3d18c59fcc25f744bd22cba09d8cc089aa
Muhammad Awais <mrmuhammadawais3@gmail.com>
2024-05-29 09:41:21 +0500

    initial commit

--- src/components/about.js
+++ src/components/about.js
@@ -1,40 +1,38 @@
-
-import { Container, Row, Col, Nav } from "react-bootstrap";
-
-import rightAbout from '../assets/img/rightAbout.png'
-import foregroundblue from '../assets/img/foregroundblue.png'
-import Logo from '../assets/img/Logo.png'
-export const About=()=>{
-    return(
-       
-    <Container>
- 
-    <Row>
-    <Col >
+import { Container, Col } from "react-bootstrap";
+import bgImage from "../assets/img/aboutBg.png";
+import Logo from "../assets/img/Logo.png";
+export const About = () => {
+  return (
     <div className="about">
- 
-      <Col className="grid-container1" >
-      <img src={foregroundblue} alt='image1' class="grid-item image1"/>
-      </Col>
-      <Col className="grid-container1">
-   <img src={rightAbout} alt='image2' class="grid-item image2"/>
-   
-   <Col className="aboutsection"> 
+      <Container>
+        <img
+          src={bgImage}
+          alt="backgroundimage"
+          className="about-background-image"
+        />
+        <div className="about-section-container">
+          <Col>
+            <p>Strategische E-mailmarketing</p>
+            <h2>Versterk uw Merk</h2>
 
-  <p>Strategische E-mailmarketing</p>
- <h2>Versterk uw Merk</h2>
-                    
-     <h1>Dankzij onze gerichte aanpak en voortdurende optimalisatie van e-mailcampagnes, kunnen wij onze klanten een hoge mate van betrokkenheid en conversie garanderen. We begrijpen de dynamiek van de markt en passen onze strategieën voortdurend aan om ervoor te zorgen dat uw boodschap relevant en impactvol blijft. Met Favoriete Acties als uw partner in e-mailmarketing, bent u verzekerd van een betrouwbare en effectieve manier om uw doelgroep te bereiken en uw merk te versterken.</h1>
-     
-<Col><img className="aboutlogo" src={Logo} alt="picsss"></img>
- </Col>
-   <h10>Uw acties, onze expertise - Succes in elke e-mai</h10></Col>
-   </Col>
-      </div>
-      </Col>
-      <Col sm={4}></Col>
-    </Row>
-    </Container>
-    
-    )
-}
+            <h1>
+              Dankzij onze gerichte aanpak en voortdurende optimalisatie van
+              e-mailcampagnes, kunnen wij onze klanten een hoge mate van
+              betrokkenheid en conversie garanderen. We begrijpen de dynamiek
+              van de markt en passen onze strategieën voortdurend aan om ervoor
+              te zorgen dat uw boodschap relevant en impactvol blijft. Met
+              Favoriete Acties als uw partner in e-mailmarketing, bent u
+              verzekerd van een betrouwbare en effectieve manier om uw doelgroep
+              te bereiken en uw merk te versterken.
+            </h1>
+
+            <Col>
+              <img className="aboutlogo" src={Logo} alt="picsss"></img>
+            </Col>
+            <h6>Uw acties, onze expertise - Succes in elke e-mai</h6>
+          </Col>
+        </div>
+      </Container>
+    </div>
+  )
+};
